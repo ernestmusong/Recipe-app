dc70bb1ce4fa58d4f577cc6d02355cf038c83034
brhanuhailu <hailubrhanu29@gmail.com>
2023-06-27 05:32:38 -0700

    recipes food controllers

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -6,7 +6,6 @@ class RecipeFoodsController < ApplicationController
     @recipe_foods = RecipeFood.all
   end
 
-  # GET /recipe_foods/1 or /recipe_foods/1.json
   def show; end
 
   # GET /recipe_foods/new
@@ -14,10 +13,8 @@ class RecipeFoodsController < ApplicationController
     @recipe_food = RecipeFood.new
   end
 
-  # GET /recipe_foods/1/edit
   def edit; end
 
-  # POST /recipe_foods or /recipe_foods.json
   def create
     @recipe_food = RecipeFood.new(recipe_food_params)
 
@@ -32,7 +29,6 @@ class RecipeFoodsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /recipe_foods/1 or /recipe_foods/1.json
   def update
     respond_to do |format|
       if @recipe_food.update(recipe_food_params)
@@ -45,7 +41,6 @@ class RecipeFoodsController < ApplicationController
     end
   end
 
-  # DELETE /recipe_foods/1 or /recipe_foods/1.json
   def destroy
     @recipe_food.destroy
 
@@ -57,12 +52,11 @@ class RecipeFoodsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_recipe_food
     @recipe_food = RecipeFood.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
+
   def recipe_food_params
     params.require(:recipe_food).permit(:quantity, :recipe_id, :food_id)
   end
