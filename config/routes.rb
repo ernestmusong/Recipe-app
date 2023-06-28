85ca564b6af9871cacdc4c9e7ef4ca4d57eb50a6
brhanuhailu <hailubrhanu29@gmail.com>
2023-06-28 07:33:14 -0700

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,12 @@ Rails.application.routes.draw do
     end
     resources :foods, only: [:index, :show, :edit, :update, :destroy] do
       resources :recipe_foods, only: [:index, :show, :edit, :update, :destroy]
-
     end
   end
+  
+  devise_scope :user do
+    get 'users/new', to: 'users/registrations#new', as: 'new_user'
+  end
+  
   get '/sign_out_user', to: 'users#sign_out_user', as: 'sign_out_user'
 end
