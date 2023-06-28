f42280295af89db0fac32fa38ddd8db526d2e533
Musong <ernestakeh@gmail.com>
2023-06-28 12:24:29 +0100

    Add general shopping list

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,5 +1,7 @@
 class ShoppingListsController < ApplicationController
   def index
-    @foods = Food.all
-   end
+    @users = User.all
+    @user = User.find(params[:user_id])
+    @foods = Food.where.not(user_id: @user.id)
+  end
 end
